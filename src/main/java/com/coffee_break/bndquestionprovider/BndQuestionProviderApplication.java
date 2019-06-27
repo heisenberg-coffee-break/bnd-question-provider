c3f37efea2ce6a84791f518d7038276a5a027974
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2019-06-27 08:23:37 +0200

    CB - Initial spring boot structure

---
+++ src/main/java/com/coffee_break/bndquestionprovider/BndQuestionProviderApplication.java
@@ -0,0 +1,13 @@
+package com.coffee_break.bndquestionprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BndQuestionProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BndQuestionProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffee_break/bndquestionprovider/BndQuestionProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coffee_break.bndquestionprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BndQuestionProviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
